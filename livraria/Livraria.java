121cfd82b36caae0367034e01db566430d1a97c5
Fernandes <eng.fernandes_br@ieee.org>
2024-09-09 09:53:58 -0300

    implementação dos getters e setters dos atributos privados

--- livraria/Livraria.java
+++ livraria/Livraria.java
@@ -1,9 +1,16 @@
 package livraria;
 public class Livraria{
     public static void main(String[] args) {
-        Livro livro1 = new Livro();
-        livro1.titulo = "Percy Jackson";
-        System.out.println(livro1.titulo);
-        livro1.cadastrar();
+       // Livro livro1 = new Livro();
+       // livro1.titulo = "Percy Jackson";
+       // System.out.println(livro1.titulo);
+       // livro1.cadastrar();
+       Livro livro2 = new Livro("Harry ...", 50.99);
+       livro2.cadastrar(); 
+
+       Livro livro3 = new Livro();
+       Livro livro4 = new Livro("Harry ..", 500, 50.99);
+       livro4.setTitulo("Percy");
+       System.out.println(livro4.getTitulo());
     }
 }
--- livraria/Livro.java
+++ livraria/Livro.java
@@ -1,9 +1,35 @@
 package livraria;
 
 public class Livro{
-    String titulo;
-    int numeroPaginas;
-    double preco;
+    private String titulo;
+    public int numeroPaginas;
+    private double preco;
+
+    public Livro(String titulo, int numeroPaginas, double preco){
+        this.titulo = titulo;
+        this.preco = preco;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public Livro(){
+
+    }
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public Livro(String titulo, double preco){
+        this.titulo = titulo;
+        this.preco = preco;
+    }
+
+
+    
 
     void cadastrar(){
         System.out.println(" Cadastrando o livro");
